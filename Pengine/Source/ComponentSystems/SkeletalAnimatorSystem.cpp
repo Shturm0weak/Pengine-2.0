50796733928b3d4abb07259585dee8639031a06a
Shturm0weak <turlak01@mail.ru>
2026-01-16 01:43:11 +0300

    Refactor Skeletal Animator System

--- Pengine/Source/ComponentSystems/SkeletalAnimatorSystem.cpp
+++ Pengine/Source/ComponentSystems/SkeletalAnimatorSystem.cpp
@@ -6,7 +6,7 @@
 #include "../Core/ThreadPool.h"
 #include "../Core/Timer.h"
 
-#include <barrier>
+#include <future>
 
 using namespace Pengine;
 
@@ -18,26 +18,21 @@ void SkeletalAnimatorSystem::OnUpdate(const float deltaTime, std::shared_ptr<Sce
 		return;
 	}
 
-	std::atomic<int> finishedCount = 0;
-	std::atomic<bool> finishedAllTasks = false;
+	std::vector<std::future<void>> futures;
 	glm::mat4 identity = glm::mat4(1.0f);
-	const int skeletalAnimatorCount = view.size();
 	for (const entt::entity entity : view)
 	{
-		ThreadPool::GetInstance().EnqueueAsync([this, &finishedCount, &finishedAllTasks, skeletalAnimatorCount, scene, entity, &identity, deltaTime]()
+		futures.emplace_back(ThreadPool::GetInstance().EnqueueAsyncFuture([this, scene, entity, &identity, deltaTime]()
 		{
 			SkeletalAnimator& skeletalAnimator = scene->GetRegistry().get<SkeletalAnimator>(entity);
 			Transform& transform = scene->GetRegistry().get<Transform>(entity);
 			std::shared_ptr<Entity> topEntity = transform.GetEntity()->GetTopEntity();
 			skeletalAnimator.UpdateAnimation(topEntity, deltaTime, identity);
-
-			if (finishedCount.fetch_add(1) + 1 == skeletalAnimatorCount)
-			{
-				finishedAllTasks.store(true);
-				finishedAllTasks.notify_one();
-			}
-		});
+		}));
 	}
 
-	finishedAllTasks.wait(false);
+	for (auto& future : futures)
+	{
+		future.get();
+	}
 }
